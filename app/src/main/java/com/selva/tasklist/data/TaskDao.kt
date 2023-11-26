971290e00ebca11182a163c5818936047b891c47
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-11-26 12:31:01 +0500

    taskdao modified

--- app/src/main/java/com/selva/tasklist/data/TaskDao.kt
+++ app/src/main/java/com/selva/tasklist/data/TaskDao.kt
@@ -2,9 +2,15 @@ package com.selva.tasklist.data
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Update
 
 @Dao
 interface TaskDao {
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insert(task: Task)
+
+    @Update
+    suspend fun update(task: Task)
+    //4 video 6:37
 }
