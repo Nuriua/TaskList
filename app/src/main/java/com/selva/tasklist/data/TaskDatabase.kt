300888533a4d63bd23f8b1dd361882086fd7a15a
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-11-27 13:53:33 +0500

    appmodule modified

--- app/src/main/java/com/selva/tasklist/data/TaskDatabase.kt
+++ app/src/main/java/com/selva/tasklist/data/TaskDatabase.kt
@@ -2,8 +2,15 @@ package com.selva.tasklist.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 @Database(entities = [Task::class], version = 1)
 abstract class TaskDatabase: RoomDatabase() {
     abstract fun taskDao(): TaskDao
+
+    class Callback(): RoomDatabase.Callback(){
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+        }
+    }
 }
--- app/src/main/java/com/selva/tasklist/di/AppModule.kt
+++ app/src/main/java/com/selva/tasklist/di/AppModule.kt
@@ -3,14 +3,22 @@ package com.selva.tasklist.di
 import android.app.Application
 import androidx.room.Room
 import com.selva.tasklist.data.TaskDatabase
+import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import javax.inject.Singleton
 
+@Module
+@InstallIn(ApplicationComponent::class)
 object AppModule {
     @Provides
+    @Singleton
     fun provideDatabase(
         app: Application
     ) = Room.databaseBuilder(app, TaskDatabase::class.java, "task_database")
             .fallbackToDestructiveMigration()
+            .addCallback()
             .build()
 
     @Provides
