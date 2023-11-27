92ec6b98890726a0bd6f352ffcd762f4a53b0027
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-11-27 13:44:46 +0500

    appmodule modified

--- app/src/main/java/com/selva/tasklist/di/AppModule.kt
+++ app/src/main/java/com/selva/tasklist/di/AppModule.kt
@@ -1,8 +1,18 @@
 package com.selva.tasklist.di
 
+import android.app.Application
+import androidx.room.Room
+import com.selva.tasklist.data.TaskDatabase
 import dagger.Provides
 
 object AppModule {
     @Provides
-    fun provideDatabase()
+    fun provideDatabase(
+        app: Application
+    ) = Room.databaseBuilder(app, TaskDatabase::class.java, "task_database")
+            .fallbackToDestructiveMigration()
+            .build()
+
+    @Provides
+    fun provideTaskDao(db: TaskDatabase) = db.taskDao()
 }
