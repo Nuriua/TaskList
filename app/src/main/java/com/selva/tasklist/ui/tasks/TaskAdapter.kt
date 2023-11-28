df28cce7d6f451f8746ab1e76820a6314b21d464
Nuriya Akhmedshina <nyriya36@gmail.com>
2023-11-28 11:31:13 +0500

    taskadapter modified

--- app/src/main/java/com/selva/tasklist/ui/tasks/TaskAdapter.kt
+++ app/src/main/java/com/selva/tasklist/ui/tasks/TaskAdapter.kt
@@ -1,15 +1,20 @@
 package com.selva.tasklist.ui.tasks
 
+import android.widget.CheckBox
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.selva.tasklist.data.Task
 import com.selva.tasklist.databinding.ItemTaskBinding
 
-class TaskAdapter : ListAdapter<Task, > {
+class TaskAdapter : ListAdapter<Task, TaskAdapter.TaskViewHolder> {
     class TaskViewHolder(private val binding: ItemTaskBinding): RecyclerView.ViewHolder(binding.root){
         fun bind(task: Task){
             binding.apply {
-
+                checkBoxCompleted.isChecked = task.completed
+                textViewName.text = task.name
+                textViewName.paint.isStrikeThruText = task.completed
+                labelPriority.isVisible = task.important
             }
         }
     }
